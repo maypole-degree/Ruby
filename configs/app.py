17b7b55b29845c331a9141475b62993401916343
maypole-degree <maypole.degree_0j@icloud.com>
2024-12-27 14:09:36 +0300

    Updated configs

---
+++ configs/app.py
@@ -0,0 +1,1 @@
+Data Pipeline Simplicity is the ultimate sophistication. Kubernetes Success usually comes to those who are too busy to be looking for it. Success doesn’t just find you. You have to go out and get it. RabbitMQ Load Balancing Success is how high you bounce when you hit bottom.
--- configs/notes.md
+++ configs/notes.md
@@ -1,1 +1,2 @@
 Success is not in what you have, but who you are. Lambda Functions Database Deployment Distributed Systems
+Scalability Load Balancing Logging Algorithm Microservices Serverless Opportunities don't happen. You create them. Monitoring
